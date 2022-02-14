8706d156348966256c8e3a1c43a56225a8becb9c
Mathis R <98423827+Mathis-R@users.noreply.github.com>
2022-02-14 19:07:07 +0100

    Added part1 and part2

---
+++ part1.sh
@@ -0,0 +1,42 @@
+# == my arch installer == #
+# (Inspired from bugswriter/arch-linux-magic script)
+
+# Part 1 Before chrooting into the new system
+
+# Make sure to :
+# - have an internet connection
+# - be booted in efi boot mode
+# - having your keyboard layout configure correctly
+
+printf '\033c'
+echo "Welcome to Mathis-R's archlinux installer"
+timedatectl set-ntp true
+lsblk
+echo "Enter the drive: "
+read drive
+cfdisk $drive 
+echo "Enter the linux partition: "
+read partition
+mkfs.ext4 $partition
+echo "Enter EFI partition: "
+read efipartition
+mkfs.fat -F 32 $efipartition
+read -p "Did you also create efi partition? [y/n] " answer
+if [[ $answer = y ]] ; then
+  echo "Enter Swap partition: "
+  read swap-partition
+  swapon $swap-partition
+fi
+mount $partition /mnt
+mkdir /mnt/boot
+mount $efipartition /mnt/boot
+echo "Enter your country (using reflector for better download speed): "
+read country
+reflector --country $country --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist 
+pacstrap /mnt base base-devel linux linux-firmware
+genfstab -U /mnt >> /mnt/etc/fstab
+cp part2.sh /mnt/part2.sh 
+chmod +x /mnt/part2.sh
+arch-chroot /mnt
+./part2.sh
+exit 
---
+++ part2.sh
@@ -0,0 +1,51 @@
+# == my arch installer == #
+# (Inspired from bugswriter/arch-linux-magic script)
+
+# Part 2 After chrooting into the new system
+# When the script finishes you have a completely working archlinux
+# You now need to set up users and download a DE / WM
+
+printf '\033c'
+ls /usr/share/zoneinfo/
+echo "Enter your region: "
+read region
+ls /usr/share/zoneinfo/$region
+echo "Enter your city: "
+read city
+ln -sf /usr/share/zoneinfo/$region/$city /etc/localtime
+hwclock --systohc
+pacman -Syy --noconfirm sed vim
+echo "Press enter, then uncomment your locale"
+read waiting
+vim /etc/locale.conf
+locale-gen
+printf '\033c'
+echo "Enter your locale: "
+read locale
+echo "LANG=$locale" > /etc/locale.conf
+echo "Enter your keymap: "
+read keymap
+echo "KEYMAP=$keymap" > /etc/vconsole.conf
+echo "Enter the hostname of the computer where your are installing archlinux: "
+read hostname
+echo $hostname > /etc/hostname
+echo "127.0.0.1       localhost" >> /etc/hosts
+echo "::1             localhost" >> /etc/hosts
+echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
+mkinitcpio -P
+passwd
+pacman --noconfirm -S grub efibootmgr os-prober
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm -S networkmanager
+systemctl enable NetworkManager
+echo "Enter your username: "
+read username
+useradd -m -G wheel $username
+passwd $username
+echo "Press enter, then allow your user / group to use the sudo command"
+read waiting
+EDITOR=vim visudo
+echo "Done ! You can now reboot, and then install a DE, WM and the software you want"
+echo "Have fun"
