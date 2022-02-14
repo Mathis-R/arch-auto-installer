8987963ae38900a48259990904ece37bb4b309d2
Mathis R <98423827+Mathis-R@users.noreply.github.com>
2022-02-14 23:33:25 +0100

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -35,7 +35,7 @@ echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
 mkinitcpio -P
 passwd
 pacman --noconfirm -S grub efibootmgr os-prober
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=2/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 pacman --noconfirm -S networkmanager
